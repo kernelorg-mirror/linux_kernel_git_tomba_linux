a9017da87d7b7349e960906a3fec00cd175477e3
Sakari Ailus <sakari.ailus@linux.intel.com>
2019-03-28 21:05:38 +0100

    media: entity: Use pad as a starting point for graph walk
    With the upcoming use of the recently added has_route() media entity op, all
    the pads in an entity will no longer be considered interconnected. This has
    an effect where the media graph is traversed: the starting pad does make a
    difference.
    
    Prepare for this change by using pad instead of the entity as an argument
    for the graph walk operations. The actual graph traversal algorithm change
    is in further patches.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Reviewed-by: Jacopo Mondi <jacopo+renesas@jmondi.org>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- include/media/media-entity.h
+++ include/media/media-entity.h
@@ -910,22 +910,20 @@ bool media_entity_has_route(struct media_entity *entity, unsigned int sink,
 			    unsigned int source);
 
 /**
- * media_graph_walk_start - Start walking the media graph at a
- *	given entity
+ * media_graph_walk_start - Start walking the media graph at a given pad
  *
  * @graph: Media graph structure that will be used to walk the graph
- * @entity: Starting entity
+ * @pad: Starting pad
  *
  * Before using this function, media_graph_walk_init() must be
  * used to allocate resources used for walking the graph. This
  * function initializes the graph traversal structure to walk the
- * entities graph starting at the given entity. The traversal
+ * entities graph starting at the given pad. The traversal
  * structure must not be modified by the caller during graph
  * traversal. After the graph walk, the resources must be released
  * using media_graph_walk_cleanup().
  */
-void media_graph_walk_start(struct media_graph *graph,
-			    struct media_entity *entity);
+void media_graph_walk_start(struct media_graph *graph, struct media_pad *pad);
 
 /**
  * media_graph_walk_next - Get the next entity in the graph

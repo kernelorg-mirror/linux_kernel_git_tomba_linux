ee341a534bab09eac7576ae2a537dd3b218334b7
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2019-03-28 21:05:55 +0100

    v4l: subdev: Add [GS]_ROUTING subdev ioctls and operations
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    
    - Add sink and source streams for multiplexed links
    - Copy the argument back in case of an error. This is needed to let the
      caller know the number of routes.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    
    - Expand and refine documentation.
    - Make the 'routes' pointer a __u64 __user pointer so that a compat32
      version of the ioctl is not required.
    - Add struct v4l2_subdev_krouting to be used for subdevice operations.
    
    Signed-off-by: Jacopo Mondi <jacopo+renesas@jmondi.org>

--- include/media/v4l2-subdev.h
+++ include/media/v4l2-subdev.h
@@ -651,6 +651,20 @@ struct v4l2_subdev_pad_config {
 };
 
 /**
+ * struct v4l2_subdev_krouting - subdev routing table
+ *
+ * @routes: &struct v4l2_subdev_route
+ * @num_routes: number of routes
+ *
+ * This structure is used to translate argument received from
+ * VIDIOC_SUBDEV_G/S_ROUTING() ioctl to sudev device drivers operations.
+ */
+struct v4l2_subdev_krouting {
+	struct v4l2_subdev_route *routes;
+	unsigned int num_routes;
+};
+
+/**
  * struct v4l2_subdev_pad_ops - v4l2-subdev pad level operations
  *
  * @init_cfg: initialize the pad config to default values
@@ -711,6 +725,10 @@ struct v4l2_subdev_pad_config {
  *		     applied to the hardware. The operation shall fail if the
  *		     pad index it has been called on is not valid or in case of
  *		     unrecoverable failures.
+ *
+ * @get_routing: get the subdevice routing table.
+ * @set_routing: enable or disable data connection routes described in the
+ *		 subdevice routing table.
  */
 struct v4l2_subdev_pad_ops {
 	int (*init_cfg)(struct v4l2_subdev *sd,
@@ -755,6 +773,10 @@ struct v4l2_subdev_pad_ops {
 			       struct v4l2_mbus_config *config);
 	int (*set_mbus_config)(struct v4l2_subdev *sd, unsigned int pad,
 			       struct v4l2_mbus_config *config);
+	int (*get_routing)(struct v4l2_subdev *sd,
+			   struct v4l2_subdev_krouting *route);
+	int (*set_routing)(struct v4l2_subdev *sd,
+			   struct v4l2_subdev_krouting *route);
 };
 
 /**

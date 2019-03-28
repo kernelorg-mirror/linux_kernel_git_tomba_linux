4db6ba9749d31f20a39cc0f69275d4a92593d1cc
Sakari Ailus <sakari.ailus@linux.intel.com>
2019-03-28 21:05:48 +0100

    media: entity: Skip link validation for pads to which there is no route to
    Links are validated along the pipeline which is about to start streaming.
    Not all the pads in entities that are traversed along that pipeline are
    part of the pipeline, however. Skip the link validation for such pads,
    and while at there rename "other_pad" to "local_pad" to convey the fact
    the route to be checked is internal to the entity.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Signed-off-by: Jacopo Mondi <jacopo+renesas@jmondi.org>

--- drivers/media/mc/mc-entity.c
+++ drivers/media/mc/mc-entity.c
@@ -482,12 +482,17 @@ __must_check int __media_pipeline_start(struct media_pad *pad,
 		bitmap_fill(has_no_links, entity->num_pads);
 
 		list_for_each_entry(link, &entity->links, list) {
-			struct media_pad *other_pad =
+			struct media_pad *local_pad =
 				link->sink->entity == entity ?
 				link->sink : link->source;
 
+			/* Ignore pads to which there is no route. */
+			if (!media_entity_has_route(entity, pad->index,
+						    local_pad->index))
+				continue;
+
 			/* Mark that a pad is connected by a link. */
-			bitmap_clear(has_no_links, other_pad->index, 1);
+			bitmap_clear(has_no_links, local_pad->index, 1);
 
 			/*
 			 * Pads that either do not need to connect or
@@ -496,13 +501,13 @@ __must_check int __media_pipeline_start(struct media_pad *pad,
 			 */
 			if (!(pad->flags & MEDIA_PAD_FL_MUST_CONNECT) ||
 			    link->flags & MEDIA_LNK_FL_ENABLED)
-				bitmap_set(active, other_pad->index, 1);
+				bitmap_set(active, local_pad->index, 1);
 
 			/*
 			 * Link validation will only take place for
 			 * sink ends of the link that are enabled.
 			 */
-			if (link->sink != other_pad ||
+			if (link->sink != local_pad ||
 			    !(link->flags & MEDIA_LNK_FL_ENABLED))
 				continue;
 

0757eb803bde8c6f21c00e93192a0f37c8d5097b
Luca Ceresoli <luca@lucaceresoli.net>
2019-07-23 22:37:21 +0200

    media: dt-bindings: add DS90UB953-Q1 video serializer
    Describe the Texas Instruments DS90UB953-Q1, a video serializer with remote
    access to I2C and GPIOs.
    
    Signed-off-by: Luca Ceresoli <luca@lucaceresoli.net>

---
+++ include/dt-bindings/media/ds90ub953.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/**
+ * Definitions for the Texas Instruments DS90UB953-Q1 video serializer
+ *
+ * Copyright (c) 2019 Luca Ceresoli <luca@lucaceresoli.net>
+ */
+
+#ifndef _DS90UB953_H
+#define _DS90UB953_H
+
+#define DS90_GPIO_FUNC_UNUSED             0
+#define DS90_GPIO_FUNC_INPUT              1
+#define DS90_GPIO_FUNC_OUTPUT_REMOTE      2
+#define DS90_GPIO_N_FUNCS                 3
+
+#endif /* _DS90UB953_H */

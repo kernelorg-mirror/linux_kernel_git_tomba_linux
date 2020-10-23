6e2ae06cc0f6847a374cc0dee72fb68005aba066
Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
2020-10-23 16:08:10 +0200

    media: fix kernel-doc markups
    Some identifiers have different names between their prototypes
    and the kernel-doc markup. Seome seems to be due to cut-and-paste
    related issues.
    
    Others need to be fixed, as kernel-doc markups should use this format:
            identifier - description
    
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com> # IPU3 and V4L2
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    (cherry picked from commit b064945517ee368bfb6343bf3fb4613d537c4bbb)

--- include/media/v4l2-mediabus.h
+++ include/media/v4l2-mediabus.h
@@ -145,7 +145,7 @@ v4l2_fill_pix_format(struct v4l2_pix_format *pix_fmt,
 }
 
 /**
- * v4l2_fill_pix_format - Ancillary routine that fills a &struct
+ * v4l2_fill_mbus_format - Ancillary routine that fills a &struct
  *	v4l2_mbus_framefmt from a &struct v4l2_pix_format and a
  *	data format code.
  *
@@ -168,7 +168,7 @@ static inline void v4l2_fill_mbus_format(struct v4l2_mbus_framefmt *mbus_fmt,
 }
 
 /**
- * v4l2_fill_pix_format - Ancillary routine that fills a &struct
+ * v4l2_fill_pix_format_mplane - Ancillary routine that fills a &struct
  *	v4l2_pix_format_mplane fields from a media bus structure.
  *
  * @pix_mp_fmt:	pointer to &struct v4l2_pix_format_mplane to be filled
@@ -188,7 +188,7 @@ v4l2_fill_pix_format_mplane(struct v4l2_pix_format_mplane *pix_mp_fmt,
 }
 
 /**
- * v4l2_fill_pix_format - Ancillary routine that fills a &struct
+ * v4l2_fill_mbus_format_mplane - Ancillary routine that fills a &struct
  *	v4l2_mbus_framefmt from a &struct v4l2_pix_format_mplane.
  *
  * @mbus_fmt:	pointer to &struct v4l2_mbus_framefmt to be filled

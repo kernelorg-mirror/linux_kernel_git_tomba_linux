7ea918225539b8283404452471d31091804ed772
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-01-13 14:34:05 +0200

    HACK: cal-camerarx: hardcode rate

--- drivers/media/platform/ti-vpe/cal-camerarx.c
+++ drivers/media/platform/ti-vpe/cal-camerarx.c
@@ -49,7 +49,7 @@ static s64 cal_camerarx_get_external_rate(struct cal_camerarx *phy)
 {
 	struct v4l2_ctrl *ctrl;
 	s64 rate;
-
+/*
 	ctrl = v4l2_ctrl_find(phy->sensor->ctrl_handler, V4L2_CID_PIXEL_RATE);
 	if (!ctrl) {
 		phy_err(phy, "no pixel rate control in subdev: %s\n",
@@ -58,6 +58,8 @@ static s64 cal_camerarx_get_external_rate(struct cal_camerarx *phy)
 	}
 
 	rate = v4l2_ctrl_g_ctrl_int64(ctrl);
+*/
+	rate = 50000000;
 	phy_dbg(3, phy, "sensor Pixel Rate: %llu\n", rate);
 
 	return rate;

863e06e0a11c1d2002a8ca1bac70ff514f99d86a
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-02-01 15:22:14 +0200

    cal: DMA ctx (rename ctx->index)
    Use dma-context as the "context index".

--- drivers/media/platform/ti-vpe/cal.h
+++ drivers/media/platform/ti-vpe/cal.h
@@ -219,7 +219,7 @@ struct cal_ctx {
 
 	unsigned int		sequence;
 	struct vb2_queue	vb_vidq;
-	unsigned int		index;
+	unsigned int		dma_ctx;
 	unsigned int		ppi_ctx;
 	unsigned int		cport;
 	unsigned int		pix_proc;
@@ -240,11 +240,11 @@ extern bool cal_mc_api;
 	dev_err((cal)->dev, fmt, ##arg)
 
 #define ctx_dbg(level, ctx, fmt, arg...)				\
-	cal_dbg(level, (ctx)->cal, "ctx%u: " fmt, (ctx)->index, ##arg)
+	cal_dbg(level, (ctx)->cal, "ctx%u: " fmt, (ctx)->dma_ctx, ##arg)
 #define ctx_info(ctx, fmt, arg...)					\
-	cal_info((ctx)->cal, "ctx%u: " fmt, (ctx)->index, ##arg)
+	cal_info((ctx)->cal, "ctx%u: " fmt, (ctx)->dma_ctx, ##arg)
 #define ctx_err(ctx, fmt, arg...)					\
-	cal_err((ctx)->cal, "ctx%u: " fmt, (ctx)->index, ##arg)
+	cal_err((ctx)->cal, "ctx%u: " fmt, (ctx)->dma_ctx, ##arg)
 
 #define phy_dbg(level, phy, fmt, arg...)				\
 	cal_dbg(level, (phy)->cal, "phy%u: " fmt, (phy)->instance, ##arg)

8e96ddaa34ab2ad5c660d7f4da8058d7c013e9a0
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-02-04 10:14:29 +0200

    media: i2c: add DS90UB960 driver
    Add driver for TI DS90UB960 FPDLink-3 Deserializer.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

---
+++ include/media/i2c/ds90ub9xx.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __MEDIA_I2C_DS90UB9XX_H__
+#define __MEDIA_I2C_DS90UB9XX_H__
+
+#include <linux/types.h>
+
+struct i2c_atr;
+
+struct ds90ub9xx_platform_data {
+	u32 port;
+	struct i2c_atr *atr;
+	unsigned long bc_rate;
+};
+
+#endif /* __MEDIA_I2C_DS90UB9XX_H__ */

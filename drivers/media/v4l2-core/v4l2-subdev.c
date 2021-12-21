daeb0c32f2d49d113b82bbfa1c268639a0d58da9
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2021-12-21 12:23:13 +0200

    media: subdev: add v4l2_subdev_get_fmt() helper function
    Add v4l2_subdev_get_fmt() helper function which implements
    v4l2_subdev_pad_ops.get_fmt using active state. Subdev drivers that
    support active state and do not need to do anything special in their
    get_fmt op can use this helper directly for v4l2_subdev_pad_ops.get_fmt.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- drivers/media/v4l2-core/v4l2-subdev.c
+++ drivers/media/v4l2-core/v4l2-subdev.c
@@ -1029,6 +1029,24 @@ void v4l2_subdev_cleanup(struct v4l2_subdev *sd)
 }
 EXPORT_SYMBOL_GPL(v4l2_subdev_cleanup);
 
+int v4l2_subdev_get_fmt(struct v4l2_subdev *sd, struct v4l2_subdev_state *state,
+			struct v4l2_subdev_format *format)
+{
+	struct v4l2_mbus_framefmt *fmt;
+
+	if (format->pad >= sd->entity.num_pads)
+		return -EINVAL;
+
+	fmt = v4l2_subdev_get_try_format(sd, state, format->pad);
+	if (!fmt)
+		return -EINVAL;
+
+	format->format = *fmt;
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(v4l2_subdev_get_fmt);
+
 #endif /* CONFIG_MEDIA_CONTROLLER */
 
 void v4l2_subdev_init(struct v4l2_subdev *sd, const struct v4l2_subdev_ops *ops)

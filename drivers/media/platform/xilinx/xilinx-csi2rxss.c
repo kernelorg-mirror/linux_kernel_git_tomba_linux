49a0a30d6dc078f6af939299ab32c2f9c1fa20c2
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2022-01-21 03:56:24 +0200

    media: xilinx: Drop legacy .s_stream() compatibility
    Now that all drivers have switched to the .enable_streams() and
    .disable_streams() operations, drop the unused .s_stream()
    implementation.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/media/platform/xilinx/xilinx-csi2rxss.c
+++ drivers/media/platform/xilinx/xilinx-csi2rxss.c
@@ -796,10 +796,6 @@ static const struct v4l2_subdev_core_ops xcsi2rxss_core_ops = {
 	.log_status = xcsi2rxss_log_status,
 };
 
-static const struct v4l2_subdev_video_ops xcsi2rxss_video_ops = {
-	.s_stream = xvip_s_stream,
-};
-
 static const struct v4l2_subdev_pad_ops xcsi2rxss_pad_ops = {
 	.enum_mbus_code = xcsi2rxss_enum_mbus_code,
 	.get_fmt = v4l2_subdev_get_fmt,
@@ -812,7 +808,6 @@ static const struct v4l2_subdev_pad_ops xcsi2rxss_pad_ops = {
 
 static const struct v4l2_subdev_ops xcsi2rxss_ops = {
 	.core = &xcsi2rxss_core_ops,
-	.video = &xcsi2rxss_video_ops,
 	.pad = &xcsi2rxss_pad_ops
 };
 

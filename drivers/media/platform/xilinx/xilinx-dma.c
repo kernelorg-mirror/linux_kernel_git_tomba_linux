20d3db8f82e9779ab02dcae536d6651aa16075d0
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2022-02-01 07:44:57 +0200

    media: xilinx: dma: Rename xvip_pipeline_validate()
    The xvip_pipeline_validate() function performs initialization more than
    validation. Rename it accordingly.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/media/platform/xilinx/xilinx-dma.c
+++ drivers/media/platform/xilinx/xilinx-dma.c
@@ -222,8 +222,8 @@ done:
 	return ret;
 }
 
-static int xvip_pipeline_validate(struct xvip_pipeline *pipe,
-				  struct xvip_dma *start)
+static int xvip_pipeline_init(struct xvip_pipeline *pipe,
+			      struct xvip_dma *start)
 {
 	struct media_pipeline_pad_iter iter;
 	unsigned int num_inputs = 0;
@@ -301,7 +301,7 @@ static int xvip_pipeline_prepare(struct xvip_pipeline *pipe,
 
 	/* If we're the first user validate and initialize the pipeline. */
 	if (pipe->use_count == 0) {
-		ret = xvip_pipeline_validate(pipe, dma);
+		ret = xvip_pipeline_init(pipe, dma);
 		if (ret < 0) {
 			__xvip_pipeline_cleanup(pipe);
 			goto done;

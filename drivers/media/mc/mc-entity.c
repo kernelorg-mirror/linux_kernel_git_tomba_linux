9a5287ec8dc06255ecd5c32f66fe4f8f9740a76d
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2022-07-26 15:21:39 +0300

    media: mc: entity: Rename streaming_count -> start_count
    'streaming_count' is a bit misleading name, as the count is increased
    with media_pipeline_start(). Let's rename it to 'start_count' instead.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- drivers/media/mc/mc-entity.c
+++ drivers/media/mc/mc-entity.c
@@ -414,8 +414,8 @@ __must_check int __media_pipeline_start(struct media_entity *entity,
 	struct media_link *link;
 	int ret;
 
-	if (pipe->streaming_count) {
-		pipe->streaming_count++;
+	if (pipe->start_count) {
+		pipe->start_count++;
 		return 0;
 	}
 
@@ -498,7 +498,7 @@ __must_check int __media_pipeline_start(struct media_entity *entity,
 		}
 	}
 
-	pipe->streaming_count++;
+	pipe->start_count++;
 
 	return 0;
 
@@ -551,7 +551,7 @@ void __media_pipeline_stop(struct media_entity *entity)
 	if (WARN_ON(!pipe))
 		return;
 
-	if (--pipe->streaming_count)
+	if (--pipe->start_count)
 		return;
 
 	media_graph_walk_start(graph, entity);

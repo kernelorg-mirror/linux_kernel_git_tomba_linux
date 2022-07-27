7cad917b2cd4d60b549c8cd8a56ad2e7a96656fb
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2022-07-27 10:17:42 +0300

    media: mc: entity: add alloc variant of pipeline_start
    Add new variant of media_pipeline_start(), media_pipeline_alloc_start().
    
    media_pipeline_alloc_start() can be used by drivers that do not need to
    extend the media_pipeline. The function will either use the pipeline
    already associated with the entity, if such exists, or allocate a new
    pipeline.
    
    When media_pipeline_stop() is called and the pipeline's use count drops
    to zero, the pipeline is automatically freed.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- drivers/media/mc/mc-entity.c
+++ drivers/media/mc/mc-entity.c
@@ -529,6 +529,8 @@ void __media_pipeline_stop(struct media_entity *entity)
 
 	media_graph_walk_cleanup(graph);
 
+	if (pipe->allocated)
+		kfree(pipe);
 }
 EXPORT_SYMBOL_GPL(__media_pipeline_stop);
 
@@ -541,6 +543,45 @@ void media_pipeline_stop(struct media_entity *entity)
 	mutex_unlock(&mdev->graph_mutex);
 }
 EXPORT_SYMBOL_GPL(media_pipeline_stop);
+
+__must_check int media_pipeline_alloc_start(struct media_entity *entity)
+{
+	struct media_device *mdev = entity->graph_obj.mdev;
+	struct media_pipeline *pipe;
+	int ret;
+	bool new_pipe = false;
+
+	mutex_lock(&mdev->graph_mutex);
+
+	/*
+	 * Is the entity already part of a pipeline? If not, we need to allocate
+	 * a pipe.
+	 */
+	pipe = media_entity_pipeline(entity);
+	if (!pipe) {
+		pipe = kzalloc(sizeof(*pipe), GFP_KERNEL);
+
+		if (!pipe) {
+			ret = -ENOMEM;
+			goto out;
+		}
+
+		new_pipe = true;
+		pipe->allocated = true;
+	}
+
+	ret = __media_pipeline_start(entity, pipe);
+	if (ret) {
+		if (new_pipe)
+			kfree(pipe);
+	}
+
+out:
+	mutex_unlock(&mdev->graph_mutex);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(media_pipeline_alloc_start);
 
 /* -----------------------------------------------------------------------------
  * Links management
--- include/media/media-entity.h
+++ include/media/media-entity.h
@@ -104,6 +104,7 @@ struct media_graph {
  * @graph:		Media graph walk during pipeline start / stop
  */
 struct media_pipeline {
+	bool allocated;
 	int start_count;
 	struct media_graph graph;
 };
@@ -1027,6 +1028,19 @@ void media_pipeline_stop(struct media_entity *entity);
  * .. note:: This is the non-locking version of media_pipeline_stop()
  */
 void __media_pipeline_stop(struct media_entity *entity);
+
+/**
+ * media_pipeline_alloc_start - Mark a pipeline as streaming
+ * @entity: Starting entity
+ *
+ * media_pipeline_alloc_start() is similar to media_pipeline_start() but instead
+ * of working on a given pipeline the function will use an existing pipeline if
+ * the entity is already part of a pipeline, or allocate a new pipeline.
+ *
+ * Calls to media_pipeline_alloc_start() must be matched with
+ * media_pipeline_stop().
+ */
+__must_check int media_pipeline_alloc_start(struct media_entity *entity);
 
 /**
  * media_devnode_create() - creates and initializes a device node interface

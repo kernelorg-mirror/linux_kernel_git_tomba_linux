860be0c600c2eb28f63e00b4898100181c4c92c6
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2023-10-25 12:03:48 +0300

    HACK: check if DSS is runtime-active when touching regs

--- drivers/gpu/drm/tidss/tidss_dispc.c
+++ drivers/gpu/drm/tidss/tidss_dispc.c
@@ -411,13 +411,22 @@ struct dispc_device {
 	struct dispc_errata errata;
 };
 
+static void CH(struct dispc_device *dispc)
+{
+	WARN_ON((dispc->dev->power.runtime_status != RPM_ACTIVE) &&
+		(dispc->dev->power.runtime_status != RPM_RESUMING) &&
+		(dispc->dev->power.runtime_status != RPM_SUSPENDING));
+}
+
 static void dispc_write(struct dispc_device *dispc, u16 reg, u32 val)
 {
+	CH(dispc);
 	iowrite32(val, dispc->base_common + reg);
 }
 
 static u32 dispc_read(struct dispc_device *dispc, u16 reg)
 {
+	CH(dispc);
 	return ioread32(dispc->base_common + reg);
 }
 
@@ -426,6 +435,7 @@ void dispc_vid_write(struct dispc_device *dispc, u32 hw_plane, u16 reg, u32 val)
 {
 	void __iomem *base = dispc->base_vid[hw_plane];
 
+	CH(dispc);
 	iowrite32(val, base + reg);
 }
 
@@ -433,6 +443,7 @@ static u32 dispc_vid_read(struct dispc_device *dispc, u32 hw_plane, u16 reg)
 {
 	void __iomem *base = dispc->base_vid[hw_plane];
 
+	CH(dispc);
 	return ioread32(base + reg);
 }
 
@@ -441,6 +452,7 @@ static void dispc_ovr_write(struct dispc_device *dispc, u32 hw_videoport,
 {
 	void __iomem *base = dispc->base_ovr[hw_videoport];
 
+	CH(dispc);
 	iowrite32(val, base + reg);
 }
 
@@ -448,6 +460,7 @@ static u32 dispc_ovr_read(struct dispc_device *dispc, u32 hw_videoport, u16 reg)
 {
 	void __iomem *base = dispc->base_ovr[hw_videoport];
 
+	CH(dispc);
 	return ioread32(base + reg);
 }
 
@@ -456,6 +469,7 @@ static void dispc_vp_write(struct dispc_device *dispc, u32 hw_videoport,
 {
 	void __iomem *base = dispc->base_vp[hw_videoport];
 
+	CH(dispc);
 	iowrite32(val, base + reg);
 }
 
@@ -463,6 +477,7 @@ static u32 dispc_vp_read(struct dispc_device *dispc, u32 hw_videoport, u16 reg)
 {
 	void __iomem *base = dispc->base_vp[hw_videoport];
 
+	CH(dispc);
 	return ioread32(base + reg);
 }
 

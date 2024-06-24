43c8546dd9301c2ccd312743b9a95ad92a1aedba
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2024-06-24 16:17:47 +0300

    CHECK: drivers/media/platform/xilinx/xilinx-demosaic.c: Subdev state fixes. Most drivers need more conversions.

--- drivers/media/platform/xilinx/xilinx-demosaic.c
+++ drivers/media/platform/xilinx/xilinx-demosaic.c
@@ -139,8 +139,7 @@ static struct v4l2_mbus_framefmt
 
 	switch (which) {
 	case V4L2_SUBDEV_FORMAT_TRY:
-		get_fmt = v4l2_subdev_get_try_format(&xdmsc->xvip.subdev,
-						     sd_state, pad);
+		get_fmt = v4l2_subdev_state_get_format(sd_state, pad);
 		break;
 	case V4L2_SUBDEV_FORMAT_ACTIVE:
 		get_fmt = &xdmsc->formats[pad];
@@ -251,10 +250,10 @@ static int xdmsc_open(struct v4l2_subdev *subdev, struct v4l2_subdev_fh *fh)
 	struct xdmsc_dev *xdmsc = to_xdmsc(subdev);
 	struct v4l2_mbus_framefmt *format;
 
-	format = v4l2_subdev_get_try_format(subdev, fh->state, XVIP_PAD_SINK);
+	format = v4l2_subdev_state_get_format(fh->state, XVIP_PAD_SINK);
 	*format = xdmsc->default_formats[XVIP_PAD_SINK];
 
-	format = v4l2_subdev_get_try_format(subdev, fh->state, XVIP_PAD_SOURCE);
+	format = v4l2_subdev_state_get_format(fh->state, XVIP_PAD_SOURCE);
 	*format = xdmsc->default_formats[XVIP_PAD_SOURCE];
 	return 0;
 }
@@ -359,7 +358,7 @@ static int xdmsc_probe(struct platform_device *pdev)
 	v4l2_subdev_init(subdev, &xdmsc_ops);
 	subdev->dev = &pdev->dev;
 	subdev->internal_ops = &xdmsc_internal_ops;
-	strlcpy(subdev->name, dev_name(&pdev->dev), sizeof(subdev->name));
+	strscpy(subdev->name, dev_name(&pdev->dev), sizeof(subdev->name));
 	subdev->flags |= V4L2_SUBDEV_FL_HAS_DEVNODE;
 
 	/* Default Formats Initialization */

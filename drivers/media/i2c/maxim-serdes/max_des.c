analogdevicesinc/linux

drivers: media: i2c: maxim-serdes: Store subdev resolution and field
Signed-off-by: Bogdan Togorean <bogdan.togorean@analog.com>

drivers/media/i2c/maxim-serdes/max_des.c
@@ -419,6 +419,7 @@ static int max_des_get_fmt(struct v4l2_subdev *sd,
 	if (!sd_priv->fmt)
 		return -EINVAL;
 
+	format->format = sd_priv->framefmt;
 	format->format.code = sd_priv->fmt->code;
 
 	return 0;
@@ -452,10 +453,13 @@ static int max_des_set_fmt(struct v4l2_subdev *sd,
 		return -EINVAL;
 
 	fmt = max_format_by_code(format->format.code);
-	if (!fmt)
+	if (!fmt){
+		v4l2_err(sd, "Wrong format requested: %d", format->format.code);
 		return -EINVAL;
+	}
 
 	sd_priv->fmt = fmt;
+	sd_priv->framefmt = format->format;
 
 	v4l2_ctrl_s_ctrl_int64(sd_priv->pixel_rate_ctrl,
 			       max_des_get_pixel_rate(sd_priv));

drivers/media/i2c/maxim-serdes/max_ser.c
@@ -23,6 +23,7 @@ struct max_ser_subdev_priv {
 	struct max_ser_priv *priv;
 	const struct max_format *fmt;
 	const char *label;
+	struct v4l2_mbus_framefmt framefmt;
 
 	struct media_pad pads[MAX_SER_PAD_NUM];
 
@@ -268,6 +269,7 @@ static int max_ser_get_fmt(struct v4l2_subdev *sd,
 	if (!sd_priv->fmt)
 		return -EINVAL;
 
+	format->format = sd_priv->framefmt;
 	format->format.code = sd_priv->fmt->code;
 
 	return 0;
@@ -288,10 +290,13 @@ static int max_ser_set_fmt(struct v4l2_subdev *sd,
 		return -EINVAL;
 
 	fmt = max_format_by_code(format->format.code);
-	if (!fmt)
+	if (!fmt){
+		v4l2_err(sd, "Wrong format requested: %d", format->format.code);
 		return -EINVAL;
+	}
 
 	sd_priv->fmt = fmt;
+	sd_priv->framefmt = format->format;
 
 	mutex_lock(&priv->lock);
 

kernelorg-mirror/linux_kernel_git_tomba_linux

drivers/media/i2c/maxim-serdes/max_des.c
@@ -33,6 +33,7 @@ struct max_des_channel {
 
 	struct max_des_priv *priv;
 	const struct max_format *fmt;
+	struct v4l2_mbus_framefmt framefmt;
 	const char *label;
 
 	struct media_pad pads[MAX_DES_PAD_NUM];
@@ -545,6 +546,7 @@ static int max_des_get_fmt(struct v4l2_subdev *sd,
 	if (!channel->fmt)
 		return -EINVAL;
 
+	format->format = channel->framefmt;
 	format->format.code = channel->fmt->code;
 
 	return 0;
@@ -578,10 +580,13 @@ static int max_des_set_fmt(struct v4l2_subdev *sd,
 		return -EINVAL;
 
 	fmt = max_format_by_code(format->format.code);
-	if (!fmt)
+	if (!fmt){
+		v4l2_err(sd, "Wrong format requested: %d", format->format.code);
 		return -EINVAL;
+	}
 
 	channel->fmt = fmt;
+	channel->framefmt = format->format;
 
 	v4l2_ctrl_s_ctrl_int64(channel->pixel_rate_ctrl,
 			       max_des_get_pixel_rate(channel));

drivers/media/i2c/maxim-serdes/max_ser.c
@@ -31,6 +31,7 @@ struct max_ser_channel {
 
 	struct max_ser_priv *priv;
 	const struct max_format *fmt;
+	struct v4l2_mbus_framefmt framefmt;
 	const char *label;
 
 	struct media_pad pads[MAX_SER_PAD_NUM];
@@ -225,6 +226,7 @@ static int max_ser_get_fmt(struct v4l2_subdev *sd,
 	if (!channel->fmt)
 		return -EINVAL;
 
+	format->format = channel->framefmt;
 	format->format.code = channel->fmt->code;
 
 	return 0;
@@ -241,10 +243,13 @@ static int max_ser_set_fmt(struct v4l2_subdev *sd,
 		return -EINVAL;
 
 	fmt = max_format_by_code(format->format.code);
-	if (!fmt)
+	if (!fmt){
+		v4l2_err(sd, "Wrong format requested: %d", format->format.code);
 		return -EINVAL;
+	}
 
 	channel->fmt = fmt;
+	channel->framefmt = format->format;
 
 	return 0;
 }
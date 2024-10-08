555c7b301df5f8f0389c826511ff26c6d64d1604
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2024-10-08 16:55:24 +0300

    drm/fourcc: Add DRM_FORMAT_XV15/XV20
    Add two new pixel formats:
    
    DRM_FORMAT_XV15 ("XV15")
    DRM_FORMAT_XV20 ("XV20")
    
    The formats are 2 plane 10 bit per component YCbCr, with the XV15 2x2
    subsampled whereas XV20 is 2x1 subsampled.
    
    Upstream gstreamer supports these formats with the format names of
    NV12_10LE32 and NV16_10LE32. However upstream gstreamer kmssink does not
    support these formats. Xilinx has kmssink commits here which add the
    support:
    
    https://github.com/Xilinx/gst-plugins-bad.git xlnx-rebase-v1.18.5
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- include/uapi/drm/drm_fourcc.h
+++ include/uapi/drm/drm_fourcc.h
@@ -305,6 +305,17 @@ extern "C" {
 #define DRM_FORMAT_BGR565_A8	fourcc_code('B', '5', 'A', '8')
 
 /*
+ * 2 plane 10 bit per component YCbCr
+ * index 0 = Y plane, [31:0] x:Y2:Y1:Y0 2:10:10:10 little endian
+ * index 1 = Cb:Cr plane, [63:0] x:Cb2:Cr2:Cb1:x:Cr1:Cb0:Cr0 2:10:10:10:2:10:10:10 little endian
+ */
+/* GST_VIDEO_FORMAT_NV12_10LE32 */
+/* similar to DRM_FORMAT_P030 */
+#define DRM_FORMAT_XV15		fourcc_code('X', 'V', '1', '5') /* 2x2 subsampled Cb:Cr plane 2:10:10:10 */
+/* GST_VIDEO_FORMAT_NV16_10LE32 */
+#define DRM_FORMAT_XV20		fourcc_code('X', 'V', '2', '0') /* 2x1 subsampled Cb:Cr plane 2:10:10:10 */
+
+/*
  * 2 plane YCbCr
  * index 0 = Y plane, [7:0] Y
  * index 1 = Cr:Cb plane, [15:0] Cr:Cb little endian

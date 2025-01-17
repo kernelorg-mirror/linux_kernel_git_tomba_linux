6af762e34f081e580aa81bff98b5c434b7205eca
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2025-01-17 15:23:28 +0200

    HACK: media: rcar-vin: Fix RAW8
    On Gen4 we need to set VNMC_EXINF_RAW8 to get 8-bit RAW8.
    
    TODO: Deal with non-Gen4.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- drivers/media/platform/renesas/rcar-vin/rcar-dma.c
+++ drivers/media/platform/renesas/rcar-vin/rcar-dma.c
@@ -94,6 +94,7 @@
 #define VNMC_INF_YUV16		(5 << 16)
 #define VNMC_INF_RGB888		(6 << 16)
 #define VNMC_INF_RGB666		(7 << 16)
+#define VNMC_EXINF_RAW8		(1 << 12)
 #define VNMC_VUP		(1 << 10)
 #define VNMC_IM_ODD		(0 << 3)
 #define VNMC_IM_ODD_EVEN	(1 << 3)
@@ -812,7 +813,7 @@ static int rvin_setup(struct rvin_dev *vin)
 	case MEDIA_BUS_FMT_SGRBG8_1X8:
 	case MEDIA_BUS_FMT_SRGGB8_1X8:
 	case MEDIA_BUS_FMT_Y8_1X8:
-		vnmc |= VNMC_INF_RAW8;
+		vnmc |= VNMC_INF_RAW8 | VNMC_EXINF_RAW8;
 		break;
 	case MEDIA_BUS_FMT_SBGGR10_1X10:
 	case MEDIA_BUS_FMT_SGBRG10_1X10:

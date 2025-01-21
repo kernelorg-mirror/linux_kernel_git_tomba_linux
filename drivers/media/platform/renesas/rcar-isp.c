18d473a2b6ac51068dc8dc69d6c334fcc74574ce
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2025-01-21 15:12:03 +0200

    media: rcar-isp: Move {enable|disable}_streams() calls
    With multiple streams the operation to enable the ISP hardware and to
    call {enable|disable}_streams() on upstream subdev will need to be
    handled separately.
    
    Prepare for that by moving {enable|disable}_streams() calls out from
    risp_start() and risp_stop().
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- drivers/media/platform/renesas/rcar-isp.c
+++ drivers/media/platform/renesas/rcar-isp.c
@@ -268,18 +268,11 @@ static int risp_start(struct rcar_isp *isp, struct v4l2_subdev_state *state)
 	/* Start ISP. */
 	risp_write(isp, ISPSTART_REG, ISPSTART_START);
 
-	ret = v4l2_subdev_enable_streams(isp->remote, isp->remote_pad,
-					 BIT_ULL(0));
-	if (ret)
-		risp_power_off(isp);
-
-	return ret;
+	return 0;
 }
 
 static void risp_stop(struct rcar_isp *isp)
 {
-	v4l2_subdev_disable_streams(isp->remote, isp->remote_pad, BIT_ULL(0));
-
 	/* Stop ISP. */
 	risp_write(isp, ISPSTART_REG, ISPSTART_STOP);
 
@@ -305,6 +298,13 @@ static int risp_enable_streams(struct v4l2_subdev *sd,
 			return ret;
 	}
 
+	ret = v4l2_subdev_enable_streams(isp->remote, isp->remote_pad,
+					 BIT_ULL(0));
+	if (ret) {
+		risp_stop(isp);
+		return ret;
+	}
+
 	isp->stream_count += 1;
 
 	return ret;
@@ -321,6 +321,8 @@ static int risp_disable_streams(struct v4l2_subdev *sd,
 
 	if (!isp->remote)
 		return -ENODEV;
+
+	v4l2_subdev_disable_streams(isp->remote, isp->remote_pad, BIT_ULL(0));
 
 	if (isp->stream_count == 1)
 		risp_stop(isp);

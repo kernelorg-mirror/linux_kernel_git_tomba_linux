277b3687fb7f1fd861c5eb9d41e30d0c4cccacea
Tomi Valkeinen <tomi.valkeinen+renesas@ideasonboard.com>
2025-01-21 15:14:07 +0200

    media: rcar-csi2: Move {enable|disable}_streams() calls
    With multiple streams the operation to enable the CSI-2 hardware and to
    call {enable|disable}_streams() on upstream subdev will need to be
    handled separately.
    
    Prepare for that by moving {enable|disable}_streams() calls out from
    rcsi2_start() and rcsi2_stop().
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen+renesas@ideasonboard.com>

--- drivers/media/platform/renesas/rcar-csi2.c
+++ drivers/media/platform/renesas/rcar-csi2.c
@@ -1509,20 +1509,12 @@ static int rcsi2_start(struct rcar_csi2 *priv, struct v4l2_subdev_state *state)
 		return ret;
 	}
 
-	ret = v4l2_subdev_enable_streams(priv->remote, priv->remote_pad,
-					 BIT_ULL(0));
-	if (ret) {
-		rcsi2_enter_standby(priv);
-		return ret;
-	}
-
 	return 0;
 }
 
 static void rcsi2_stop(struct rcar_csi2 *priv)
 {
 	rcsi2_enter_standby(priv);
-	v4l2_subdev_disable_streams(priv->remote, priv->remote_pad, BIT_ULL(0));
 }
 
 static int rcsi2_enable_streams(struct v4l2_subdev *sd,
@@ -1542,6 +1534,13 @@ static int rcsi2_enable_streams(struct v4l2_subdev *sd,
 		ret = rcsi2_start(priv, state);
 		if (ret)
 			return ret;
+	}
+
+	ret = v4l2_subdev_enable_streams(priv->remote, priv->remote_pad,
+					 BIT_ULL(0));
+	if (ret) {
+		rcsi2_stop(priv);
+		return ret;
 	}
 
 	priv->stream_count += 1;
@@ -1564,6 +1563,8 @@ static int rcsi2_disable_streams(struct v4l2_subdev *sd,
 
 	if (priv->stream_count == 1)
 		rcsi2_stop(priv);
+
+	v4l2_subdev_disable_streams(priv->remote, priv->remote_pad, BIT_ULL(0));
 
 	priv->stream_count -= 1;
 

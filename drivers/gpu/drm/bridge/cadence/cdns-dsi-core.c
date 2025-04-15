e4398d29cc2f76a27bf7ed72176f53c3c9f16ef0
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2025-04-15 11:15:23 +0300

    drm/bridge: cdns-dsi: Fix modesetting issue wrt. uninit
    The driver currently sets link_initialized and phy_initialized to false
    in cdns_dsi_suspend(). As the phy won't be reconfigured unless it's in
    uninitialized state, this means that the videomode cannot be changed
    without the driver going to suspend. And suspend doesn't necessarily
    happen between modesets, causing black screen as the PLL uses a wrong
    clock rate.
    
    The driver is also missing calls to phy_power_off() and phy_exit().
    
    Add the necessary code to cdns_dsi_bridge_atomic_late_disable() to
    disable the phy and link, fixing the issue.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- drivers/gpu/drm/bridge/cadence/cdns-dsi-core.c
+++ drivers/gpu/drm/bridge/cadence/cdns-dsi-core.c
@@ -608,6 +608,11 @@ static void cdns_dsi_bridge_atomic_late_disable(struct drm_bridge *bridge,
 	if (dsi->platform_ops && dsi->platform_ops->disable)
 		dsi->platform_ops->disable(dsi);
 
+	dsi->phy_initialized = false;
+	dsi->link_initialized = false;
+	phy_power_off(dsi->dphy);
+	phy_exit(dsi->dphy);
+
 	pm_runtime_put(dsi->base.dev);
 }
 
@@ -1249,8 +1254,6 @@ static int __maybe_unused cdns_dsi_suspend(struct device *dev)
 	clk_disable_unprepare(dsi->dsi_sys_clk);
 	clk_disable_unprepare(dsi->dsi_p_clk);
 	reset_control_assert(dsi->dsi_p_rst);
-	dsi->link_initialized = false;
-	dsi->phy_initialized = false;
 	return 0;
 }
 

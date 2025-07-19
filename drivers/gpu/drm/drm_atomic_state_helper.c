a84b1aa750fb09dfcdb946ea2af3f5c3dd9b2b8c
Maxime Ripard <mripard@kernel.org>
2025-07-19 08:39:17 +0200

    drm/atomic_state_helper: Fix bridge state initialization
    Bridges implement their state using a drm_private_obj and an
    hand-crafted reset implementation.
    
    Since drm_private_obj doesn't have a set of reset helper like the other
    states, __drm_atomic_helper_bridge_reset() was initializing both the
    drm_private_state and the drm_bridge_state structures.
    
    This initialization however was missing the drm_private_state.obj
    pointer to the drm_private_obj the state was allocated for, creating a
    NULL pointer dereference when trying to access it.
    
    Fixes: 751465913f04 ("drm/bridge: Add a drm_bridge_state object")
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/drm_atomic_state_helper.c
+++ drivers/gpu/drm/drm_atomic_state_helper.c
@@ -713,6 +713,13 @@ void drm_atomic_helper_connector_destroy_state(struct drm_connector *connector,
 }
 EXPORT_SYMBOL(drm_atomic_helper_connector_destroy_state);
 
+static void __drm_atomic_helper_private_obj_reset(struct drm_private_obj *obj,
+						  struct drm_private_state *state)
+{
+	memset(state, 0, sizeof(*state));
+	state->obj = obj;
+}
+
 /**
  * __drm_atomic_helper_private_obj_duplicate_state - copy atomic private state
  * @obj: CRTC object
@@ -802,6 +809,7 @@ void __drm_atomic_helper_bridge_reset(struct drm_bridge *bridge,
 				      struct drm_bridge_state *state)
 {
 	memset(state, 0, sizeof(*state));
+	__drm_atomic_helper_private_obj_reset(&bridge->base, &state->base);
 	state->bridge = bridge;
 }
 EXPORT_SYMBOL(__drm_atomic_helper_bridge_reset);

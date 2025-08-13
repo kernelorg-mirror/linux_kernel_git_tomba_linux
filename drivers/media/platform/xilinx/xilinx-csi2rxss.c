c7880c25d64709f406727d50ec7440b05a114663
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2025-08-13 15:23:26 +0300

    media: xilinx: csi2rx: Support operation without vfb
    VFB is an optional video stream formatter inside the Xilinx CSI-2 RX
    subsystem. VFB converts a design-time-chosen CSI-2 pixel format to
    Xilinx's AXI4-Stream video format. In other words, with VFB, CSI-2 RX
    subsystem will output AXI4-Stream video stream, and without VFB, CSI-2
    RX subsystem will output AXI4-Stream containing the "raw" CSI-2 pixel
    data.
    
    We have xlnx,vfb property in the DT to indicate the existence of VFB.
    For some reason the driver checks the property and refuses to probe if
    it's not present. I see nothing in the driver that depends on VFB, and
    everything I have tested works fine without VFB.
    
    So drop the requirement to have VFB. This allows the user to, e.g.,
    capture MIPI RAW10 directly, and also to have pipelines which allow
    changing the pixel format.
    
    Note that VFB is still often required if the pipeline has Xilinx video
    processing IPs after the CSI-2 RX IP.
    
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

--- drivers/media/platform/xilinx/xilinx-csi2rxss.c
+++ drivers/media/platform/xilinx/xilinx-csi2rxss.c
@@ -807,7 +807,7 @@ static int xcsi2rxss_parse_of(struct xcsi2rxss_state *xcsi2rxss)
 	struct v4l2_fwnode_endpoint vep = {
 		.bus_type = V4L2_MBUS_CSI2_DPHY
 	};
-	bool en_csi_v20, vfb;
+	bool en_csi_v20;
 	int ret;
 
 	en_csi_v20 = of_property_read_bool(node, "xlnx,en-csi-v2-0");
@@ -852,12 +852,6 @@ static int xcsi2rxss_parse_of(struct xcsi2rxss_state *xcsi2rxss)
 	if (ret < 0) {
 		dev_err(dev, "invalid csi-pxl-format property!\n");
 		return ret;
-	}
-
-	vfb = of_property_read_bool(node, "xlnx,vfb");
-	if (!vfb) {
-		dev_err(dev, "operation without VFB is not supported\n");
-		return -EINVAL;
 	}
 
 	ep = fwnode_graph_get_endpoint_by_id(dev_fwnode(dev),

db2dbea564bd19ec62dcc22d2f6682ce5b10410b
Maxime Ripard <mripard@kernel.org>
2025-09-02 10:12:30 +0200

    drm/atomic: Only call atomic_destroy_state on a !NULL pointer
    The drm_atomic_state structure is freed through the
    drm_atomic_state_put() function, that eventually calls
    drm_atomic_state_default_clear() by default when there's no active
    users of that state.
    
    It then iterates over all objects with a state, and will call the
    atomic_destroy_state callback on the state pointer. The state pointer is
    mostly used these days to point to which of the old or new state needs
    to be freed, depending on whether the state was committed or not.
    
    So it all makes sense.
    
    However, with the hardware state readout support approaching, we might
    have a state, with multiple objects in it, but no state to free because
    we want them to persist. In such a case, the state pointer is going to
    be NULL, and thus we'll end up with NULL pointer dereference.
    
    In order to make it work, let's first test if the state pointer isn't
    NULL before calling atomic_destroy_state on it.
    
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -265,8 +265,10 @@ void drm_atomic_state_default_clear(struct drm_atomic_state *state)
 		if (!connector)
 			continue;
 
-		connector->funcs->atomic_destroy_state(connector,
-						       state->connectors[i].state_to_destroy);
+		if (state->connectors[i].state_to_destroy)
+			connector->funcs->atomic_destroy_state(connector,
+							       state->connectors[i].state_to_destroy);
+
 		state->connectors[i].ptr = NULL;
 		state->connectors[i].state_to_destroy = NULL;
 		state->connectors[i].old_state = NULL;
@@ -280,8 +282,9 @@ void drm_atomic_state_default_clear(struct drm_atomic_state *state)
 		if (!crtc)
 			continue;
 
-		crtc->funcs->atomic_destroy_state(crtc,
-						  state->crtcs[i].state_to_destroy);
+		if (state->crtcs[i].state_to_destroy)
+			crtc->funcs->atomic_destroy_state(crtc,
+							  state->crtcs[i].state_to_destroy);
 
 		state->crtcs[i].ptr = NULL;
 		state->crtcs[i].state_to_destroy = NULL;
@@ -300,8 +303,10 @@ void drm_atomic_state_default_clear(struct drm_atomic_state *state)
 		if (!plane)
 			continue;
 
-		plane->funcs->atomic_destroy_state(plane,
-						   state->planes[i].state_to_destroy);
+		if (state->planes[i].state_to_destroy)
+			plane->funcs->atomic_destroy_state(plane,
+							   state->planes[i].state_to_destroy);
+
 		state->planes[i].ptr = NULL;
 		state->planes[i].state_to_destroy = NULL;
 		state->planes[i].old_state = NULL;
@@ -328,8 +333,10 @@ void drm_atomic_state_default_clear(struct drm_atomic_state *state)
 		if (!obj)
 			continue;
 
-		obj->funcs->atomic_destroy_state(obj,
-						 state->private_objs[i].state_to_destroy);
+		if (state->private_objs[i].state_to_destroy)
+			obj->funcs->atomic_destroy_state(obj,
+							 state->private_objs[i].state_to_destroy);
+
 		state->private_objs[i].ptr = NULL;
 		state->private_objs[i].state_to_destroy = NULL;
 		state->private_objs[i].old_state = NULL;

463d8002699dc55c122398b0940b14f0272fb1b4
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2026-03-20 14:34:50 +0200

    Revert "tisci pd debug print"
    This reverts commit 07ed1a332d901128ca718412eef93d37ed746ddf.

--- drivers/pmdomain/ti/ti_sci_pm_domains.c
+++ drivers/pmdomain/ti/ti_sci_pm_domains.c
@@ -112,8 +112,6 @@ static int ti_sci_pd_power_off(struct generic_pm_domain *domain)
 	struct ti_sci_pm_domain *pd = genpd_to_ti_sci_pd(domain);
 	const struct ti_sci_handle *ti_sci = pd->parent->ti_sci;
 
-	printk("POWER OFF %u\n", pd->idx);
-
 	return ti_sci->ops.dev_ops.put_device(ti_sci, pd->idx);
 }
 
@@ -125,8 +123,6 @@ static int ti_sci_pd_power_on(struct generic_pm_domain *domain)
 {
 	struct ti_sci_pm_domain *pd = genpd_to_ti_sci_pd(domain);
 	const struct ti_sci_handle *ti_sci = pd->parent->ti_sci;
-
-	printk("POWER ON %u\n", pd->idx);
 
 	if (pd->exclusive)
 		return ti_sci->ops.dev_ops.get_device_exclusive(ti_sci,
